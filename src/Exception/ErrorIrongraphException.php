81ff8e3e69b04e550ae9de7d0389c1e221d79081
Jose Cabrera <jcabrera@bienparabien.com>
2021-10-03 11:53:24 -0500

    Se agrega metodo getHttpCode() a ErrorIrongraphException

--- src/Exception/ErrorIrongraphException.php
+++ src/Exception/ErrorIrongraphException.php
@@ -6,14 +6,20 @@ use Exception;
 class ErrorIrongraphException extends Exception
 {
 	protected $response;
+	protected $httpCode;
 
-	public function __construct($message, $response=null, $code=0, Exception $previus=null) {
+	public function __construct($message, $response=null, $httpCode=0, Exception $previus=null) {
 		$this->response = $response;
+		$this->httpCode = $httpCode;
 
-		parent::__construct($message, $code, $previus);
+		parent::__construct($message, $httpCode, $previus);
 	}
 
 	public function getResponse() {
 		return $this->response;
+	}
+
+	public function getHttpCode() {
+		return $this->httpCode;
 	}
 }
