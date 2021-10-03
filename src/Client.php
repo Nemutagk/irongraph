f7539aabaa2999020122e92d3a5134ed3683b0e3
Jose Cabrera <jcabrera@bienparabien.com>
2021-10-03 01:07:28 -0500

    Se reintegra la excepción ErrorIrongrapException, se cambia el metodo getError por getResponse

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,6 @@ namespace BienParaBien\GraphqlClient;
 use Log;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Exception\{ClientException,RequestException,ServerException};
-use BienParaBien\GraphqlClient\Exception\ErrorGraphQLException;
 
 class Client
 {
---
+++ src/Exception/ErrorIrongraphException.php
@@ -0,0 +1,19 @@
+<?php
+namespace BienParaBien\GraphqlClient\Exception;
+
+use Exception;
+
+class ErrorIrongraphException extends Exception
+{
+	protected $response;
+
+	public function __construct($message, $response=null, $code=0, Exception $previus=null) {
+		$this->response = $response;
+
+		parent::__construct($message, $code, $previus);
+	}
+
+	public function getResponse() {
+		return $this->response;
+	}
+}
